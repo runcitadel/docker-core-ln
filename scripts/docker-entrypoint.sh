27dcc9622da72f89124233e1f72ed4a00f61416a
Aaron Dewes <aaron.dewes@protonmail.com>
2022-02-04 12:06:20 +0100

    Remove modifications from entrypoint

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-# Original file (Upstream)
-# https://raw.githubusercontent.com/ElementsProject/lightning/master/tools/docker-entrypoint.sh
-# modified to include plugin path for building  my custom container
-
 : "${EXPOSE_TCP:=false}"
 
 networkdatadir="${LIGHTNINGD_DATA}/${LIGHTNINGD_NETWORK}"
-HTTP_RPC_PLUGIN_PATH="/rust-plugin/c-lightning-http-plugin/target/release/c-lightning-http-plugin"
 
 if [ "$EXPOSE_TCP" == "true" ]; then
     set -m
-    lightningd --plugin="${HTTP_RPC_PLUGIN_PATH}" "$@" &
+    lightningd "$@" &
 
     echo "C-Lightning starting"
     while read -r i; do if [ "$i" = "lightning-rpc" ]; then break; fi; done \
@@ -22,5 +17,5 @@ if [ "$EXPOSE_TCP" == "true" ]; then
     socat "TCP4-listen:$LIGHTNINGD_RPC_PORT,fork,reuseaddr" "UNIX-CONNECT:${networkdatadir}/lightning-rpc" &
     fg %-
 else
-    exec lightningd --network="${LIGHTNINGD_NETWORK}" --plugin="${HTTP_RPC_PLUGIN_PATH}" "$@"
+    exec lightningd --network="${LIGHTNINGD_NETWORK}" "$@"
 fi
